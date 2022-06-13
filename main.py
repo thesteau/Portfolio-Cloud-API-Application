6c43f4cd892e181e35ef6290be21cd47b87f2cdb
Steven Au <25945183+thesteau@users.noreply.github.com>
2022-06-12 21:56:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# Steven Au
+# CS 493
+# Final Project
+from flask import Flask, jsonify
+from src import load, boats, auth, users, welcome
+
+app = Flask(__name__)
+app.register_blueprint(welcome.bp)
+app.register_blueprint(users.bp)
+app.register_blueprint(boats.bp)
+app.register_blueprint(load.bp)
+app.register_blueprint(auth.bp)
+
+app.config['SECRET_KEY'] = 'someSecret493'
+
+
+@app.errorhandler(auth.AuthError)
+def handle_auth_error(ex):
+    return jsonify(ex.error), ex.status_code
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
